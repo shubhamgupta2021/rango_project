0b31225b48d3aa4143f587e406c9ce4a3c760218
shubhamgupta2021 <shubham.gupta2021@gmail.com>
2015-06-11 20:24:23 +0530

    base html added

---
+++ rango/migrations/0001_initial.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(unique=True, max_length=180)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Page',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(unique=True, max_length=255)),
+                ('views', models.IntegerField(default=0)),
+                ('url', models.URLField()),
+                ('category', models.ForeignKey(to='rango.Category')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
